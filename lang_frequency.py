37f5839420ba07c9625be923d90ecde8cee91d9c
Artem Aleksashkin <aaleksashkin@iponweb.net>
2017-08-01 21:26:16 +0300

    draft

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,4 +1,6 @@
-
+import sys
+import re
+from pprint import pprint
 
 def load_data(filepath):
     pass
@@ -9,4 +11,24 @@ def get_most_frequent_words(text):
 
 
 if __name__ == '__main__':
-    pass
+    dictionary = {}
+    text_chunk = u''
+    with open('text.txt', 'r', encoding='utf-8') as file_descriptor:
+        while True:
+            buff = file_descriptor.read(100)
+            if not buff:
+                break
+            text_chunk = text_chunk + buff
+            while True:
+                re_match = re.match('\S+\s+', text_chunk)
+                if re_match:
+                    word = re_match.group(0).strip()
+                    text_chunk = text_chunk[len(re_match.group(0)):]
+                    if word in dictionary:
+                        dictionary[word] += 1
+                    else:
+                        dictionary[word] = 1
+                else:
+                    break
+    file_descriptor.close()
+    print(dictionary)
